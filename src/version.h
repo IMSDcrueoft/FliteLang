24a3507b217ab94b592d27037fb827d17804b131
IMSDcrueoft <imsd_personal@outlook.com>
2025-05-11 15:44:45 +0800

    version.h

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2025 IMSDcrueoft (https://github.com/IMSDcrueoft)
  * See LICENSE file in the root directory for full license text.
 */
-#define INTERPRETER_VERSION "1.0.0"
+#define INTERPRETER_VERSION "1.0.1"
 #define INTERPRETER_NAME "FliteLang"
 #define INTERPRETER_COPYRIGHT "2025"
 #define INTERPRETER_DEVELOPER "IMSDcrueoft"
